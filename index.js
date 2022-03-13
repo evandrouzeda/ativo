b516a94ed89ad105c84b130fca7623449d8a2a5a
evandrouzeda <evandrouzeda@gmail.com>
2022-03-13 15:42:34 -0300

    recebendo session por parametro

--- index.js
+++ index.js
@@ -14,37 +14,60 @@ app.use(cors())
 
 const mongodb = new Mongodb()
 const getAtivos = async (request, response, next) => {
+    const { userid } = request.query
     const db = await mongodb.getDB()
-    db.collection("Ativos").find({}).toArray()
+    db.collection("Ativos").find({ usuario: new ObjectId(userid) }).toArray()
         .then(result => response.status(200).json(result))
         .catch(error => response.status(401).json({status: 'error', message: 'Erro: ' + error}))
 }
 
 const addAtivo = async (request, response, next) => {
+    const { userid } = request.query
+    const ativo = request.body
+    ativo.usuario = new ObjectId(userid)
+    ativo.carteira = ativo.carteira ? new ObjectId(ativo.carteira) : ""
     const db = await mongodb.getDB()
-    db.collection("Ativos").insertOne(request.body)
+    db.collection("Ativos").insertOne(ativo)
         .then(result => response.status(201).json({ status: 'success', result}))
         .catch(error => response.status(401).json({ status: 'error', message: 'Erro: ' + error }))
 }
 
 const updateAtivo = async (request, response, next) => {
-    const { _id, nome } = request.body
+    const { userid } = request.query
+    const ativo = request.body
+    const filter = { 
+        _id: new ObjectId(ativo._id),
+        usuario: new ObjectId(userid)
+    }
+    delete ativo._id
+    delete ativo.usuario
     const db = await mongodb.getDB()
-    db.collection("Ativos").updateOne({_id: new ObjectId(_id)}, {$set: {nome}})
+    ativo.carteira = ativo.carteira ? new ObjectId(ativo.carteira) : ""
+    db.collection("Ativos").updateOne(filter, {$set: ativo})
         .then(result => response.status(201).json({ status: 'success', message: 'Ativo atualizada.' }))
         .catch(error => response.status(401).json({ status: 'error', message: 'Erro: ' + error }))
 }
 
 const deleteAtivo = async (request, response, next) => {
+    const { userid } = request.query
+    const query = { 
+        _id: new ObjectId(request.params.id),
+        usuario: new ObjectId(userid)
+    }
     const db = await mongodb.getDB()
-    db.collection("Ativos").deleteOne({_id: new ObjectId(request.params.id)})
+    db.collection("Ativos").deleteOne(query)
         .then(result => response.status(201).json({ status: 'success', message: 'Ativo removida.' }))
         .catch(error => response.status(401).json({ status: 'error', message: 'Erro: ' + error }))
 }
 
 const getAtivoPorID = async (request, response, next) => {
+    const { userid } = request.query
+    const query = { 
+        _id: new ObjectId(request.params.id),
+        usuario: new ObjectId(userid)
+    }
     const db = await mongodb.getDB()
-    db.collection("Ativos").findOne({_id: new ObjectId(request.params.id)})
+    db.collection("Ativos").findOne(query)
         .then(result => response.status(201).json(result))
         .catch(error => response.status(401).json({ status: 'error', message: 'Erro: ' + error }))
 }
